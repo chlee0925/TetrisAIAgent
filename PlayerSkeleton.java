ba622c691a549511570984c70cae0976ae53ca11
chlee0925 <chlee0925@gmail.com>
2018-03-23 16:55:56 +0800

    Add evaluation feature templates

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,5 +1,8 @@
+import java.util.*;
 
 public class PlayerSkeleton {
+
+	ArrayList<Integer> rows = new ArrayList<>(new int[]{0,1,2,3,4,5,6,7,8,9});
 
 	public int pickMove(State s, int[][] legalMoves) {
 		return pickMoveImpl(s.getField(), legalMoves);
@@ -24,6 +27,61 @@ public class PlayerSkeleton {
 	
 	// Implementation method for pickMove
 	public int pickMoveImpl(int[][] field, int[][] legalMoves) {
+		// TODO for every possible legal move, try making that move and run the evaluation function on that state.
+		// TODO Ensure that field and levalMoves are NOT modified at the end.
 		return 0;
 	}
+
+	public int evaluationFunction(int[][] field) {
+		return
+			(20) * rewardRowsToBeCleared(field)
+			+ (-1) * (rows.stream().map(col -> featureColumnHeight(field, col)).sum())
+			+ (-1) * (rows.stream().filter(col -> {return col <= 8;}).map(col -> featureAbsoluteAdjColumnHeightDiff(field, col)).sum())
+			+ (-1) * featureMaxColumnHeight(field)
+			+ (-1) * featureNumOfHoles(field);
+	}
+
+
+	///////////////////////////////////////
+	///////////		FEATURES	///////////
+	///////////////////////////////////////
+
+	/**
+	 * FEATURE 1~10 - Column Height
+	 */
+	public int featureColumnHeight(int[][] field, int col) {
+		return 0; // TODO Implement
+	}
+
+	/**
+	 * FEATURE 11~19 - Absolute height difference between (col) and (col+1) columns.
+	 */
+	public int featureAbsoluteAdjColumnHeightDiff(int[][] field, int col) {
+		return 0; // TODO Implement
+	}
+
+	/**
+	 * FEATURE 20 - Maximum height across all columns
+	 */
+	public int featureMaxColumnHeight(int[][] field) {
+		return 0; // TODO Implement
+	}
+
+	/**
+	 * FEATURE 21 - the number of holes in the wall, that is, the number of empty positions of 
+	 * the wall that have at least one full position above them.
+	 */
+	public int featureNumOfHoles(int[][] field) {
+		return 0; // TODO Implement
+	}
+
+	/////////////////////////////////////
+	///////////		REWARD	/////////////
+	/////////////////////////////////////
+
+	public int rewardRowsToBeCleared(int[][] field) {
+		return 0; // TODO Implement
+	}
+
+
 }
