466bf87db13c2009a34d46a0b929bac9fb558a79
idawatibustan <idawati.bustan@gmail.com>
2018-04-01 21:03:11 +0800

    Limit number of turns per game

--- NoVisualPlayerSkeleton.java
+++ NoVisualPlayerSkeleton.java
@@ -5,7 +5,7 @@ public class NoVisualPlayerSkeleton {
     public static void main(String[] args) {
         PlayerSkeleton p = new PlayerSkeleton(Arrays.asList(args).stream().mapToDouble(weightStr -> Double.parseDouble(weightStr)).boxed().collect(Collectors.toCollection(ArrayList::new)));
         NoVisualState s = new NoVisualState();
-        while(!s.hasLost()) s.makeMove(p.pickMoveImpl(s.getField(), s.legalMoves(), s.getTop(), s.getpOrients(), s.getpWidth(), s.getpHeight(), s.getpBottom(), s.getpTop(), s.getNextPiece()));
+        while(!s.hasLost() && s.getTurnNumber() < 5000) s.makeMove(p.pickMoveImpl(s.getField(), s.legalMoves(), s.getTop(), s.getpOrients(), s.getpWidth(), s.getpHeight(), s.getpBottom(), s.getpTop(), s.getNextPiece()));
         System.out.println(s.getRowsCleared());
     }
 }
